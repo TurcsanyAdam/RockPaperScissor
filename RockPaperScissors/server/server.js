5eca274f8a4e1e13c057da05a216ce937b796514
Csondika <csondikaa@gmail.com>
2020-04-27 12:06:55 +0200

    Create default server. Add configuration to package.json to listen to changes.

--- RockPaperScissors/server/server.js
+++ RockPaperScissors/server/server.js
@@ -1,1 +1,24 @@
-// JavaScript source code
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+io.on('connection', (sock) => {
+    sock.emit('message', 'Connected to server');
+});
+
+const clientPath = '${__dirName}/../client';
+console.log('Serving static from ${clientPath}');
+app.use(express.static(clientPath));
+
+server.on('error', (err) => {
+    console.error('Server error: ', err);
+});
+
+server.listen(8080, () => {
+    console.log('RPS STARTED');
+});
+
