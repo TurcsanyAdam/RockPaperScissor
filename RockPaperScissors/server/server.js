2145f0c665287db76c7dde658daf2834f1ea052b
Csondika <csondikaa@gmail.com>
2020-04-27 13:06:35 +0200

    Server now emits messages to other clients.

--- RockPaperScissors/server/server.js
+++ RockPaperScissors/server/server.js
@@ -3,16 +3,24 @@ const express = require('express');
 const socketio = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-
-const io = socketio(server);
-io.on('connection', (sock) => {
-    sock.emit('message', 'Connected to server');
-});
 
 const clientPath = '${__dirName}/../client';
 console.log('Serving static from ${clientPath}');
+
 app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', (sock) => {
+    console.log('Player connected');
+    sock.emit('message', 'Connected to server');
+
+    sock.on('message', (text) => {
+        io.emit('message', text);
+    });
+});
 
 server.on('error', (err) => {
     console.error('Server error: ', err);
