7a5f48c70c55c25a601bd4b7c03adc291d47ae01
Csondika <csondikaa@gmail.com>
2020-04-29 09:42:54 +0200

    Change port to listein on.

--- RockPaperScissors/server/server.js
+++ RockPaperScissors/server/server.js
@@ -36,6 +36,6 @@ server.on('error', (err) => {
     console.error('Server error:', err);
 });
 
-server.listen(8080, () => {
+server.listen(3000, () => {
     console.log('RPS started on 8080');
 });
